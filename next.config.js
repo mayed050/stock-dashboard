5bc2efab90424d15e4f279fedda8dea1b104abf8
mayed050 <ae.mayed@gmail.com>
2025-08-13 17:35:36 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/**** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: { unoptimized: true },
-};
-module.exports = nextConfig;
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+  },
+}
+
+module.exports = nextConfig
